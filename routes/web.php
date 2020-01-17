a42da75e3cf87f353a7e63091f687c683244ae36
eggegg <eggegg01@126.com>
2020-01-17 06:53:23 +0000

    用户界面 - 检查优惠券

--- routes/web.php
+++ routes/web.php
@@ -50,6 +50,8 @@ Route::group(['middleware'=>'auth'], function(){
         Route::get('payment/{order}/alipay', 'PaymentController@payByAlipay')->name('payment.alipay');
         Route::get('payment/alipay/return', 'PaymentController@alipayReturn')->name('payment.alipay.return');
 
+        //优惠券
+        Route::get('coupon_codes/{code}', 'CouponCodesController@show')->name('coupon_codes.show');
 
     });
 });
