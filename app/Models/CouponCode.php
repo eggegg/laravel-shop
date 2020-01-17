1640cad5f6722f2f565f7cd0b15fa02f14af8a81
eggegg <eggegg01@126.com>
2020-01-17 07:14:14 +0000

    优化优惠券模块

--- app/Models/CouponCode.php
+++ app/Models/CouponCode.php
@@ -66,7 +66,7 @@ class CouponCode extends Model
         return $code;
     }
 
-    public function checkAvailable($orderAmount = null)
+    public function checkAvailable(User $user, $orderAmount = null)
     {
         if (!$this->enabled) {
             throw new CouponCodeUnavailableException('优惠券不存在');
@@ -86,6 +86,30 @@ class CouponCode extends Model
 
         if (!is_null($orderAmount) && $orderAmount < $this->min_amount) {
             throw new CouponCodeUnavailableException('订单金额不满足该优惠券最低金额');
+        }
+
+        //有关联了此优惠券的未付款且未关闭订单或者已付款且未退款订单
+        /*
+          select * from orders where user_id = xx and coupon_code_id = xx
+            and (
+                ( paid_at is null and closed = 0 )
+                or ( paid_at is not null and refund_status = 'pending' )
+            )
+         */
+        $used = Order::where('user_id', $user->id)
+            ->where('coupon_code_id', $this->id)
+            ->where(function($query) {
+                $query->where(function($query) {
+                    $query->whereNull('paid_at')
+                        ->where('closed', false);
+                })->orWhere(function($query) {
+                    $query->whereNotNull('paid_at')
+                        ->where('refund_status', Order::REFUND_STATUS_PENDING);
+                });
+            })
+            ->exists();
+        if ($used) {
+            throw new CouponCodeUnavailableException('你已经使用过这张优惠券了');
         }
     }
 
